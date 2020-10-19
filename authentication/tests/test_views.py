ada0b20c20c8bc9a30c0ce1b7a88c700ea6e9d11
Easylord <yusufisrael007@gmail.com>
2020-10-19 06:10:52 +0100

    First commit

--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -4,7 +4,6 @@ from ..models import User
 class TestViews(TestSetUp):
     def test_user_cannot_register_with_no_data(self):
         res= self.client.post(self.register_url)
-        
         self.assertEqual(res.status_code, 400)
 
 
